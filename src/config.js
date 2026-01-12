c807adb03482a82b798381b1541acbc53c7add89
xxbbm <2473515265@qq.com>
2026-01-12 17:33:33 +0800

    Fix formatting issues in config.js

--- src/config.js
+++ src/config.js
@@ -133,8 +133,8 @@ const config = {
 	//项目卡片 其中 字段"show"控制初始卡片的text是否展开
 	projectcards: [
 		{ go: "🚀 前往", img: "/img/xbmt.jpg", title: "博客", subtitle: "落魄谷中寒风吹", text: "懒得做了", url: "", show: false },
-		{ go: "🗂️ 前往", img: "/img/xbmt1.jpg", title: "赞助", subtitle: "春秋蝉鸣少年归", text: "If you see this line, I've managed to get your attention.", url: "http://183.66.27.19:53253/#", show: false },
-		{ go: "📝 前往", img: "/img/xbmt2.jpg", title: "网盘", subtitle: "荡魂山上石人泪", text: "If you see this line, I've managed to get your attention.", url: "https://woahu3yn.cn-hk1.rainapp.top/", show: false },
+		{ go: "🗂️ 前往", img: "/img/xbmt1.jpg", title: "赞助", subtitle: "春秋蝉鸣少年归", text: "If you see this line, I've managed to get your attention.", url: "http://111.170.6.79:42698/", show: false },
+		{ go: "📝 前往", img: "/img/xbmt2.jpg", title: "网盘", subtitle: "荡魂山上石人泪", text: "If you see this line, I've managed to get your attention.", url: "http://111.170.6.79:46487/", show: false },
 		{ go: "👍 前往", img: "/img/xbmt3.jpg", title: "Project 4", subtitle: "定仙游走魔向北", text: "If you see this line, I've managed to get your attention.", url: "https://github.com/xxbbm", show: false },
 		{ go: "🗃 前往", img: "/img/xbmt4.jpg", title: "Project 5", subtitle: "逆流河上万仙退", text: "If you see this line, I've managed to get your attention.", url: "https://github.com/xxbbm", show: false },
 		{ go: "🎨 前往", img: "/img/xbmt5.jpg", title: "Project 6", subtitle: "爱情不敌坚持泪", text: "If you see this line, I've managed to get your attention.", url: "https://github.com/xxbbm", show: false },
@@ -146,6 +146,7 @@ const config = {
 }
 
 export default config
+
 
 
 
